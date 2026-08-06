e3ec06f3ea0f2f7fcebdbad008973dd77d7ca155
João Pedro <joao.guadalupe06@gmail.com>
2026-08-06 01:41:37 -0300

    fix: ajuste de import errado

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,6 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-use Symfony\Component\Process\Exception\ProcessTimedOutException;
 
 class ProductController extends Controller
 {
