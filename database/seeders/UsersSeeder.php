c0148b8aa3b5b6edfa7b79c08254a80e509622e6
João Pedro <joao.guadalupe06@gmail.com>
2026-08-23 04:04:23 -0300

    fix: ajustes user factory

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
@@ -17,6 +18,8 @@ class UsersSeeder extends Seeder
             ['email' => 'admin@emporio.com'],
             User::factory()->admin()->raw([
                 'name' => 'Administrador',
+                'email' => 'admin@emporio.com',
+                'password' => Hash::make('12345678'),
             ])
         );
 
