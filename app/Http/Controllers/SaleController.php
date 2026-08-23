7ca75d542235312297268f3cc738b2afc8b7e11b
João Pedro <joao.guadalupe06@gmail.com>
2026-08-23 14:19:49 -0300

    fix: ajuste sale controller

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -26,36 +26,7 @@ class SaleController extends Controller
         return view('sales.index', compact('sales', 'graphic'));
     }
 
-    public function indexPurchaseHistory(Request $request)
-    {
-        $user = Auth::user();
 
-        $purchases = Sale::asBuyer($user)
-            ->withDetails()
-            ->period($request->input('start'), $request->input('end'))
-            ->orderByDesc('purchase_date')
-            ->paginate(8);
-        return view('purchases.history', compact('purchases'));
-    }
-
-    public function generatePdfPurchases(Request $request)
-    {
-        $user = Auth::user();
-
-        $purchases = Sale::asBuyer($user)
-            ->withDetails()
-            ->period($request->input('start'), $request->input('end'))
-            ->orderByDesc('purchase_date')
-            ->get();
-
-        $pdf = Pdf::loadView('purchases.pdf-purchases', [
-            'purchases' => $purchases,
-            'start'     => $request->input('start'),
-            'end'       => $request->input('end'),
-        ]);
-
-        return $pdf->stream('relatorio-compras.pdf');
-    }
 
 
     public function generatePDF(Request $request)
